67fd03a9065632465d223d61d874252095fb34ac
ruben <zhirubin@gmail.com>
2021-01-27 16:16:18 +0800

    init

---
+++ example/code.go
@@ -0,0 +1,9 @@
+//go:generate gen-const-str
+package code
+
+const (
+	// 参数错误
+	ErrParams = 10400
+	// 系统内部错误
+	ErrServer = 10500
+)
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rnben/gen-const-str
+
+go 1.15
+
+require github.com/pkg/errors v0.9.1
